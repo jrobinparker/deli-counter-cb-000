41b7b918500b2bb627f95d8fd41916259ad94226
Jeremy Parker <jrobinparker@gmail.com>
2018-07-02 03:49:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,8 +19,6 @@ def take_a_number(katz_deli, name)
   
   if katz_deli.any? === true
     katz_deli << name
-  else
-    katz_deli.unshift(name)
   end
   
   katz_deli.each_with_index do |name, index|
